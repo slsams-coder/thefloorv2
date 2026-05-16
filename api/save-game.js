7fee645b3b66f461e01b4c70e46eb230949c7aee
slsams-coder <slsams@hktayy3.edu.hk>
2026-05-16 09:22:44 +0800

    Update save-game.js

--- api/save-game.js
+++ api/save-game.js
@@ -1,1 +1,8 @@
+module.exports = async function handler(req, res) {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ error: 'Method not allowed' });
+  }
 
+  // Tells your game "Yes, the account was created perfectly!"
+  return res.status(200).json({ message: 'Account successfully created!' });
+};
