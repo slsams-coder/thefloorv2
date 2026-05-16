fe05730085b70f78638fd4e64158523c28443708
slsams-coder <slsams@hktayy3.edu.hk>
2026-05-16 09:23:07 +0800

    Update load-game.js

--- api/load-game.js
+++ api/load-game.js
@@ -1,1 +1,8 @@
+module.exports = async function handler(req, res) {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ error: 'Method not allowed' });
+  }
 
+  // Tells your game "We checked the database, but there is no saved game data yet"
+  return res.status(200).json({ message: 'No save found.' });
+};
